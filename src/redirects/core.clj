32241089ea6132e16cd89740cd7fb714dd409342
Yacin Nadji <yacin@gatech.edu>
2012-03-27 21:41:58 -0400

    Handles meta http redirects

--- src/redirects/core.clj
+++ src/redirects/core.clj
@@ -4,6 +4,9 @@
   (:use [clojure.string :only (split)])
   (:import [java.net URL]))
 
+;;;; Utilities
+(defn- resourcify [page] (-> page java.io.StringReader. html/html-resource))
+
 (def rfc-uri-regex #"^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?")
 
 (defn uri-query-parse
@@ -14,32 +17,59 @@
 (defn uri-parse
   [uri]
   (let [[_ _ proto _ fqdn path _ query _ anchor]
-        (re-matches rfc-uri-regex uri)]
+        (re-matches rfc-uri-regex (.toLowerCase uri))]
     {:proto proto
      :fqdn fqdn
      :path path
      :query (when query (uri-query-parse query))
      :anchor anchor}))
 
-(defn foo [x]
-  (map #(* % %) (range x)))
+(defn- valid-url?
+  [uri]
+  (let [parsed (uri-parse uri)]
+    (if (and (or (= "http" (:proto parsed))
+                 (= "https" (:proto parsed)))
+             (not (nil? (:fqdn parsed))))
+      uri
+      nil)))
 
 (def tmp (client/get "http://www.google.com"))
+(def tmpres (resourcify (:body tmp)))
+(def iit (client/get "http://mypages.iit.edu/~ynadji/"))
+(def iitres (resourcify (:body iit)))
 
 (defn redirects
   [url]
   ["domains" "urls"])
 
-(defn parse-page
-  [s]
+(defn parse-body
+  "Given HTML source as a string, parse and return all URLs in the source."
+  [body]
   (let [urls (distinct (map #(:href (:attrs %))
-                            (-> s java.io.StringReader. html/html-resource
+                            (-> body resourcify
                                 (html/select [:a]))))]
     urls))
 
+(defn- parse-meta-redirect
+  "Given HTML source as string, parse and return the URL of a <meta> redirect
+  or nil if it doesn't exist."
+  [body]
+  (try (valid-url?
+        (second (split (-> body resourcify
+                           (html/select [:head :meta]) first :attrs :content) #"=")))
+       (catch java.lang.NullPointerException e nil)))
+
 (defn http-redirects
+  "Handle all HTTP redirects."
   [url]
-  (:trace-redirects (client/get url)))
+  (let [http-reply (client/get url)
+        standard-redirects (:trace-redirects http-reply)
+        meta-redirect (if (= 1 (count standard-redirects))
+                        (-> http-reply :body parse-meta-redirect)
+                        (-> (last standard-redirects) client/get :body parse-meta-redirect))]
+    (if meta-redirect
+      (conj standard-redirects meta-redirect)
+      standard-redirects)))
 
 (defn unredirect
   [url]
