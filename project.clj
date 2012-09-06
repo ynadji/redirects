ee2b62d06763b1ae030be5477fa1cf9b1e9f1f7a
Yacin Nadji <yacin@gatech.edu>
2012-09-06 14:05:47 -0400

    Use stable clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject redirects "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.3.2"]
                  [server-socket "1.0.0"]
                  [enlive "1.0.0"]]
